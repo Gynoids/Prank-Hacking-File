cdcc635215d2c1fb30875930f0b98648aa61e12b
Gynoids <121044930+Gynoids@users.noreply.github.com>
2022-12-21 11:07:49 +0530

    Add files via upload

---
+++ Rename This FIle.bat
@@ -0,0 +1,75 @@
+@echo off
+Prank File Made By Gynoids
+color 9
+echo Would you like to scan your PC?
+pause
+echo Scanning...
+pause
+dir/s
+cls
+echo Scan Complete!
+pause
+cls
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+echo ACCESS DENIED!
+@echo off
+Timeout 2
+cls
+color 0a
+echo -----------------------------------------------------------------------------
+echo Thank You For Your Files :) Just A Saying Im Not Taking Your Files Right Now. 
+echo -----------------------------------------------------------------------------
+Timeout 2
+:1
+dir/s
+goto 1
